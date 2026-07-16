d137626640125a66be49c9c13fe9ca0f7d160079
Comparateurpro <zakariamiloudi682@gmail.com>
2026-07-16 20:24:17 +0100

    Implement search functionality for cards
    Added a search function to filter cards based on user input.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Comparateur de prix chargé !");
+function rechercher(){
+
+let recherche=document
+.getElementById("search")
+.value
+.toLowerCase();
+
+let cartes=document
+.querySelectorAll(".card");
+
+cartes.forEach(function(card){
+
+let titre=card.innerText.toLowerCase();
+
+if(titre.includes(recherche)){
+card.style.display="block";
+}else{
+card.style.display="none";
+}
+
+});
+
+}
